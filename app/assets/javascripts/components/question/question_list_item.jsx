3baed84aca00b9d6f1c2a275d8d1af68850d72d1
David Ammons <dammo001@gmail.com>
2015-10-25 16:12:57 -0700

    Arrows for upvote counter

--- app/assets/javascripts/components/question/question_list_item.jsx
+++ app/assets/javascripts/components/question/question_list_item.jsx
@@ -9,22 +9,7 @@ QuestionListItem = React.createClass({
 		window.scrollTo(0,0); 
 	},
 
-	vote: function(value){
-		params = {upvote: {id: this.props.question.id, value: value }};
-		if (this.props.question.isVoted){
-			ApiUtil.Upvote.unVoteQuestion(params);
-		} else { 
-			ApiUtil.Upvote.voteQuestion(params); 
-		}
-	},
-
 	render: function(){
-		var voteTally = 0;
-		if (this.props.question && this.props.question.upvotes){
-			this.props.question.upvotes.forEach(function(value){
-				voteTally += value; 
-			});
-		}
 		var tagNames; 
 		if (this.props.question.tags){ 
 			tagNames = (
@@ -83,8 +68,7 @@ QuestionListItem = React.createClass({
 				</div> 
 			 	<div className="answer-teaser"> {this.props.question.body} </div>
 			 	<ul className="question-options">  
-			 		<li className="question-options"> <button onClick={this.vote.bind(null, 1)} type="button" id="vote" className="btn btn-default btn-sm upvote-btn">Upvote | {voteTally} </button> </li>
-			 		<li className="question-options"> <button onClick={this.vote.bind(null, -1)} type="button" id="vote" className="btn btn-default btn-sm upvote-btn">Downvote</button>  </li>
+			 		<li className="question-options"> <Upvote question={this.props.question}/> </li> 
 			 		<li className="question-options"> <CommentForm questionId={this.props.question.id} /></li>
 			 		<li className="question-options"> {comments} </li>  
 		 		</ul> 
---
+++ app/assets/javascripts/components/upvote/upvote.js.jsx
@@ -0,0 +1,35 @@
+Upvote = React.createClass({
+
+	getInitialState: function(){
+		return({
+			upvoted: false,
+			downvoted: false
+		});
+	},
+
+	vote: function(value){
+		params = {upvote: {id: this.props.question.id, value: value }};
+		if (this.props.question.isVoted){
+			ApiUtil.Upvote.unVoteQuestion(params);
+		} else { 
+			ApiUtil.Upvote.voteQuestion(params); 
+		}
+	},
+	
+	render: function(){
+		var voteTally = 0;
+		if (this.props.question && this.props.question.upvotes){
+			this.props.question.upvotes.forEach(function(value){
+				voteTally += value; 
+			});
+		}
+
+		return (
+			<div className="upvote counter">
+			 	<span onClick={this.vote.bind(null, 1)} className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 
+			 	<span className="vote-tally">{voteTally}</span> 
+			 	<span onClick={this.vote.bind(null, -1)} className="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>
+		 	</div> 
+		 )
+	}
+})
