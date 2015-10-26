88c4312a7b8c883f214fad75f71712a86fc093e4
David Ammons <dammo001@gmail.com>
2015-10-25 23:22:38 -0700

    Fix some links

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -4,7 +4,7 @@ Footer = React.createClass({
 		<footer className="footer-distributed">
 			<div className="footer-right"> 
 			<a href="https://www.facebook.com/david.ammons.710"> <i className="fa fa-facebook-square"/> </a> 
-				<a href=""><i className="fa fa-linkedin"></i></a> 
+				<a href="https://www.linkedin.com/pub/david-ammons/87/910/33a"><i className="fa fa-linkedin"></i></a> 
 				<a href="https://github.com/dammo001"><i className="fa fa-github"></i></a>
 			</div>
 			<div className="footer-left">  
